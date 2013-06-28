5ab54fb706786c9d8629e65dbfaabb6b4dc9d1a7
codefunny <markzheng@marktekiMacBook-Pro.local>
2013-06-28 10:39:32 +0800

    fire

---
+++ UsefullCode/UsefullCode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UsefullCode
+//
+//  Created by mark zheng on 13-6-28.
+//  Copyright (c) 2013年 fullpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UsefullCode/UsefullCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UsefullCode
+//
+//  Created by mark zheng on 13-6-28.
+//  Copyright (c) 2013年 fullpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
